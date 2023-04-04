eea27a1dd140f8cb64660d386189f3d8ce28d7b4
denis_levchenko <tranzi90@gmail.com>
2023-04-04 16:53:27 +0300

    Initial commit

---
+++ src/store/user/user.reducer.ts
@@ -0,0 +1,36 @@
+import { USER_ACTION_TYPES } from './user.types';
+import { UserAction } from './user.action';
+
+import { UserData } from '../../utils/firebase/firebase.utils';
+
+export type UserState = {
+  readonly currentUser: UserData | null;
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
+const INITIAL_STATE: UserState = {
+  currentUser: null,
+  isLoading: false,
+  error: null,
+};
+
+export const userReducer = (
+  state = INITIAL_STATE,
+  action = {} as UserAction
+) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
+      return { ...state, currentUser: payload };
+    case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
+      return { ...state, currentUser: null };
+    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
+    case USER_ACTION_TYPES.SIGN_IN_FAILED:
+    case USER_ACTION_TYPES.SIGN_UP_FAILED:
+      return { ...state, error: payload };
+    default:
+      return state;
+  }
+};
